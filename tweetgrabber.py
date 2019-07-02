c010f7e631303d537a66f8e279436f012280d417
Gotham <gotham@Gothams-MacBook-Air.local>
2019-07-02 17:15:17 -0500

    made some changes

--- tweetgrabber.py
+++ tweetgrabber.py
@@ -25,20 +25,15 @@ def get_all_tweets(screen_name):
         if counter == 10:
             time.sleep(1800)
             counter = 0       
-        #this prevent duplicates
+        #prevents duplicates
         new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
         counter = counter + 1        
-        #save most recent tweets
         alltweets.extend(new_tweets)
         #update the id of the oldest tweet less one
         oldest = alltweets[-1].id - 1   
-    #transform the tweepy tweets into a 2D array that will populate the csv	
     tweetarray = [[tweet.source, tweet.text.encode("utf-8"), tweet.created_at, tweet.retweet_count, tweet.favorite_count, tweet.id_str] for tweet in alltweets]    
-    #write the csv
     with open('tweetstore_2014_2019.txt', 'wb') as f:
         writer = csv.writer(f)
         writer.writerow(["source","text","created_at","retweet_count","favorite_count","is_retweet","id_str"])
         writer.writerows(tweetarray)    
     pass
-    
-
